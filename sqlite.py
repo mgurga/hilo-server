75b9c9f95b4c4db3fe06dec7d33f99349de2adbb
mgurga <motiurga@gmail.com>
2022-09-08 13:42:47 -0700

    mostly finished sqlite database

--- sqlite.py
+++ sqlite.py
@@ -24,53 +24,121 @@ class Database:
         self.cur.execute("CREATE TABLE hashes(key, value)")
         self.cur.execute("CREATE TABLE authkeys(key, value)")
 
-    def user_exists(self, username) -> bool:
+    # User operations
+    def user_exists(self, username: str) -> bool:
         res = self.cur.execute(f"SELECT username FROM accounts WHERE username='{username}'")
         return not (res.fetchone() is None)
 
-    def create_user(self, username, password, datecreated) -> None:
+    def create_user(self, username: str, password: str, datecreated: float) -> None:
         self.cur.execute(f"INSERT INTO accounts VALUES('{username}', '{password}', {datecreated})")
         self.con.commit()
 
-    def valid_user_password(self, username, password) -> Dict:
+    def valid_user_password(self, username: str, password: str) -> Dict:
         res = self.cur.execute(f"SELECT * FROM accounts WHERE username='{username}' AND password='{password}'")
         res = res.fetchone()
+        if res is None:
+            return None
         return {"username": res[0], "password": res[1], "datecreated": res[2]}
 
-    def hash_exists(self, key, val) -> bool:
-        res = self.cur.execute(f"SELECT key FROM hashes WHERE key='{key}' AND value='{val}'")
+    # Hash operations
+    def hash_exists(self, key: str, val: str) -> bool:
+        res = self.cur.execute(f"SELECT * FROM hashes WHERE key='{key}' AND value='{val}'")
         return not (res.fetchone() is None)
 
-    def delete_hash(self, key):
+    def create_hash(self, key: str, val: str):
+        self.cur.execute(f"INSERT INTO hashes VALUES('{key}', '{val}')")
+        self.con.commit()
+
+    def delete_hash(self, key: str):
         self.cur.execute(f"DELETE FROM hashes WHERE key='{key}'")
         self.con.commit()
 
-    def create_authkey(self, key, val):
+    # Authkey operations
+    def create_authkey(self, key: str, val: str):
         self.cur.execute(f"INSERT INTO authkeys VALUES('{key}', '{val}')")
         self.con.commit()
 
-    def authkey_exists(self, key) -> bool:
+    def authkey_exists(self, key: str) -> bool:
         res = self.cur.execute(f"SELECT * FROM authkeys WHERE key='{key}'")
         return not (res.fetchone() is None)
 
-    def create_game(self, creator, id, name, description, datecreated):
-        self.cur.execute(f"INSERT INTO games VALUES('{creator}', '{id}', '{name}', '{description}', '{datecreated}')")
+    def delete_authkey(self, key: str):
+        self.cur.execute(f"DELETE FROM authkeys WHERE key='{key}'")
         self.con.commit()
 
-    def delete_game(self, creator, id):
+    # Game operations
+    def create_game(self, creator: str, id: int, name: str, description: str, datecreated: float):
+        self.cur.execute(f"INSERT INTO games VALUES('{creator}', '{id}', '{name}', '{description}', {datecreated})")
+        self.con.commit()
+
+    def delete_game(self, creator: str, id: str):
         self.cur.execute(f"DELETE FROM games WHERE creator='{creator}' AND id='{id}'")
         self.con.commit()
 
-    def user_games(self, creator) -> Dict:
+    def user_games(self, creator: str) -> Dict:
         res = self.cur.execute(f"SELECT * FROM games WHERE creator='{creator}'")
         res = res.fetchall()
-        return res
+        out = []
+        for game in res:
+            out.append({"creator": game[0], "id": game[1], "name": game[2], "description": game[3], "datecreated": float(game[4])})
+        return out
 
-    def game_exists(self, id) -> bool:
+    def game_exists(self, id: str) -> bool:
         res = self.cur.execute(f"SELECT * FROM games WHERE id='{id}'")
         return not (res.fetchone() is None)
 
-    def game_info(self, id) -> Dict:
+    def game_info(self, id: str) -> Dict:
         res = self.cur.execute(f"SELECT * FROM games WHERE id='{id}'")
         res = res.fetchone()
-        return {"creator": res[0], "id": res[1], "name": res[2], "description": res[3], "datecreated": res[4]}
+        if res is None:
+            return None
+        return {"creator": res[0], "id": res[1], "name": res[2], "description": res[3], "datecreated": float(res[4])}
+    
+    def update_game(self, id: str, name: str, desc: str):
+        res = self.cur.execute(f"SELECT * FROM games WHERE id='{id}'")
+        res = res.fetchone()
+        if res is None:
+            return None
+        self.cur.execute(f"DELETE FROM games WHERE id='{res[1]}'")
+        self.cur.execute(f"INSERT INTO games VALUES('{res[0]}', '{res[1]}', '{name}', '{desc}', {res[4]})")
+        self.con.commit()
+
+    def recent_games(self, amount: int):
+        res = self.cur.execute(f"SELECT * FROM games ORDER BY datecreated ASC")
+        res = res.fetchmany(amount)
+        if res is None:
+            return None
+        out = []
+        for game in res:
+            out.append({"creator": game[0], "id": game[1], "name": game[2], "description": game[3], "datecreated": float(game[4])})
+        return out
+
+    # Node operations
+    def create_node(self, name: str, amount: float, nid: str, parent: str, datecreated: float):
+        self.cur.execute(f"INSERT INTO nodes VALUES('{name}', {amount}, '{nid}', '{parent}', {datecreated})")
+        self.con.commit()
+
+    def delete_node(self, nid: str):
+        self.cur.execute(f"DELETE FROM nodes WHERE id='{nid}'")
+        self.con.commit()
+
+    def node_exists(self, nid: str):
+        res = self.cur.execute(f"SELECT * FROM nodes WHERE id='{nid}'")
+        return not (res.fetchone() is None)
+
+    def update_node(self, nid: str, name: str, amount: float):
+        res = self.cur.execute(f"SELECT * FROM nodes WHERE id='{nid}'")
+        res = res.fetchone()
+        if res is None:
+            return None
+        self.cur.execute(f"DELETE FROM nodes WHERE id='{res[2]}'")
+        self.cur.execute(f"INSERT INTO nodes VALUES('{name}', {amount}, '{res[2]}', '{res[3]}', {res[4]})")
+        self.con.commit()
+
+    def game_nodes(self, game_id: str, order_by="ASC"):
+        res = self.cur.execute(f"SELECT * FROM nodes WHERE parent='{game_id}' ORDER BY datecreated {order_by}")
+        res = res.fetchall()
+        out = []
+        for game in res:
+            out.append({"name": game[0], "amount": float(game[1]), "id": game[2], "parent": game[3], "datecreated": float(game[4])})
+        return out
--- tests.py
+++ tests.py
@@ -5,18 +5,78 @@ class TestDatabase(unittest.TestCase):
     def setUp(self):
         self.db = Database(DBType.MEMORY)
 
-    def test_user_create_read(self):
+    def test_user_create_read_exist(self):
         self.db.create_user("hello", "world", 12345.6789)
         res = self.db.valid_user_password("hello", "world")
         self.assertEqual(res["username"], "hello")
         self.assertEqual(res["password"], "world")
         self.assertEqual(res["datecreated"], 12345.6789)
-
-    def test_user_exists(self):
         self.assertTrue(self.db.user_exists("hello"))
 
     def test_user_does_not_exists(self):
         self.assertFalse(self.db.user_exists("randomuser"))
+        self.assertIsNone(self.db.valid_user_password("user", "pass"))
+
+    def test_hash_create_exist(self):
+        self.db.create_hash("foo", "bar")
+        self.assertTrue(self.db.hash_exists("foo", "bar"))
+    
+    def test_hash_delete(self):
+        self.db.create_hash("foo", "bar")
+        self.assertTrue(self.db.hash_exists("foo", "bar"))
+        self.db.delete_hash("foo")
+        self.assertFalse(self.db.hash_exists("foo", "bar"))
+
+    def test_game_create_exists_delete(self):
+        self.db.create_game("foo", "1D151MP0R74N7", "game", "test game", 123456.7890)
+        self.assertTrue(self.db.game_exists("1D151MP0R74N7"))
+        self.db.delete_game("foo", "1D151MP0R74N7")
+        self.assertFalse(self.db.game_exists("1D151MP0R74N7"))
+    
+    def test_game_info(self):
+        self.db.create_game("foo", "1D151MP0R74N7!", "game", "test game", 123456.7890)
+        res = self.db.game_info("1D151MP0R74N7!")
+        self.assertEqual(res, {"creator": "foo", "id": "1D151MP0R74N7!", "name": "game", "description": "test game", "datecreated": 123456.7890})
+
+    def test_game_uppdate(self):
+        self.db.create_game("foo", "1D151MP0R74N7!!", "game", "test game", 123456.7890)
+        res = self.db.game_info("1D151MP0R74N7!!")
+        self.assertEqual(res["name"], "game")
+        self.assertEqual(res["description"], "test game")
+        self.db.update_game("1D151MP0R74N7!!", "better game", "new description")
+        res = self.db.game_info("1D151MP0R74N7!!")
+        self.assertEqual(res["name"], "better game")
+        self.assertEqual(res["description"], "new description")
+
+    def test_authkey_create_delete_exists(self):
+        self.db.create_authkey("hello", "authkey")
+        self.assertTrue(self.db.authkey_exists("hello"))
+        self.db.delete_authkey("hello")
+        self.assertFalse(self.db.authkey_exists("hello"))
+
+    def test_authkey_undeclared(self):
+        self.assertFalse(self.db.authkey_exists("notdeclared"))
+        self.db.delete_authkey("notdeclared")
+
+    def test_node_create_delete_exists(self):
+        self.db.create_node("testnode", 123, "N0D31D", "P4R3N7", 12345.678)
+        self.assertTrue(self.db.node_exists("N0D31D"))
+        self.db.delete_node("N0D31D")
+        self.assertFalse(self.db.node_exists("N0D31D"))
+
+    def test_game_nodes(self):
+        self.db.create_game("joe", "123", "node test", "i love nodes!!!", 9875.4321)
+        self.db.create_node("node1", 5, "N0D31D1", "123", 9875.4333)
+        self.db.create_node("node2", 10, "N0D31D2", "123", 9875.4444)
+        self.db.create_node("node3", 15, "N0D31D3", "123", 9875.5555)
+        res = self.db.game_nodes("123")
+        self.assertEqual(res[0]["name"], "node1")
+        self.assertEqual(res[1]["name"], "node2")
+        self.assertEqual(res[2]["name"], "node3")
+        res = self.db.game_nodes("123", "DESC")
+        self.assertEqual(res[0]["name"], "node3")
+        self.assertEqual(res[1]["name"], "node2")
+        self.assertEqual(res[2]["name"], "node1")
 
 if __name__ == '__main__':
     unittest.main()
